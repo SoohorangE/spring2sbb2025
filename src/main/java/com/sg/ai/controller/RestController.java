72b0e03170de6a9a78f2df3afd756936b672add4
sooho <soho5445@g.shingu.ac.kr>
2025-04-28 16:05:32 +0900

    04-28 Question, Answer 테이블 만들기

---
+++ src/main/java/com/sg/ai/controller/RestController.java
@@ -0,0 +1,37 @@
+package com.sg.ai.controller;
+
+import com.sg.ai.domain.User;
+import org.springframework.web.bind.annotation.*;
+
+@org.springframework.web.bind.annotation.RestController
+
+public class RestController {
+    @GetMapping("/jblog")
+    public User httpGet() {
+        User finduser = User.builder()
+                .id(1)
+                .username("ai")
+                .password("222")
+                .email("ai@ai.com")
+                .build();
+        return finduser;
+    }
+
+    @PostMapping("/jblog")
+    public String httpPost(@RequestBody User user)
+    {
+        return "Post request processed" + user.toString();
+    }
+
+    @PutMapping("/jblog")
+    public String httpPut(@RequestBody User user)
+    {
+        return "Put request processed" + user.toString();
+    }
+
+    @DeleteMapping("/jblog")
+    public String httpDelete(@RequestParam int id)
+    {
+        return "Delete request processed" + id;
+    }
+}
