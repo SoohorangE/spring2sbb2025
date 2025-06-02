4d68a0021467f8802d730976468d9eb0dfe93741
sooho <soho5445@g.shingu.ac.kr>
2025-06-02 13:28:18 +0900

    -

--- src/test/java/com/sg/ai/SbbApplicationTest.java
+++ src/test/java/com/sg/ai/SbbApplicationTest.java
@@ -13,37 +13,37 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-class SbbApplicationTest {
-
-    @Autowired
-    private QuestionRepository questionRepository;
-
-    @Test
-    void testJpa()
-    {
-        Question q1 = new Question();
-        q1.setContent("오늘이 몇일 인가요");
-        q1.setSubject("처음 질문");
-        q1.setCreateDate(LocalDateTime.now());
-        questionRepository.save(q1);
-
-    }
-
-    @Autowired
-    private AnswerRepository answerRepository;
-
-    @Test
-    void testJpaa() {
-        Optional<Question> oq = this.questionRepository.findById(1);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        Answer a = new Answer();
-        a.setContent("오늘은 5월 12일 입니다");
-        a.setQuestion(q);
-        a.setCreateDate(LocalDateTime.now());
-        answerRepository.save(a);
-
-    }
-}
+//@SpringBootTest
+//class SbbApplicationTest {
+//
+//    @Autowired
+//    private QuestionRepository questionRepository;
+//
+//    @Test
+//    void testJpa()
+//    {
+//        Question q1 = new Question();
+//        q1.setContent("오늘이 몇일 인가요");
+//        q1.setSubject("처음 질문");
+//        q1.setCreateDate(LocalDateTime.now());
+//        questionRepository.save(q1);
+//
+//    }
+//
+//    @Autowired
+//    private AnswerRepository answerRepository;
+//
+//    @Test
+//    void testJpaa() {
+//        Optional<Question> oq = this.questionRepository.findById(1);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//
+//        Answer a = new Answer();
+//        a.setContent("오늘은 5월 12일 입니다");
+//        a.setQuestion(q);
+//        a.setCreateDate(LocalDateTime.now());
+//        answerRepository.save(a);
+//
+//    }
+//}
